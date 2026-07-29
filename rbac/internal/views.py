a9038df02ae79a66b9c95b7997155fbeadf02558
Janet Cobb <jacobb@redhat.com>
2026-07-29 10:11:56 -0400

    Check for query parameters

--- rbac/internal/views.py
+++ rbac/internal/views.py
@@ -2954,7 +2954,7 @@ def replicate_updated_workspaces(request):
         return JsonResponse({"field": "since", "detail": 'missing query parameter "since"'}, status=400)
 
     if "stream" not in request.GET:
-        return JsonResponse({"field": "since", "detail": 'missing query parameter "stream"'}, status=400)
+        return JsonResponse({"field": "stream", "detail": 'missing query parameter "stream"'}, status=400)
 
     since = request.GET["since"]
     stream = request.GET["stream"]
